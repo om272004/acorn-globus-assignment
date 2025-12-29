b5c2c4ec6dd0ae4fd153835a6f7e1ac6d54e9204
om272004 <omshimpi5@gmail.com>
2025-12-29 19:18:35 +0530

    fixed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { Sidebar } from './components/SideBar'
+import { Sidebar } from './components/Sidebar'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
 
